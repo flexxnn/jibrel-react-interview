1228aa5e655b97470d64d1fa89dd638ae7a860a9
Michael Maslov <flexxnn@gmail.com>
2018-06-19 01:24:03 +0300

    list styles

--- client/src/components/MainPage/RequestList.jsx
+++ client/src/components/MainPage/RequestList.jsx
@@ -7,26 +7,35 @@ import { List, AutoSizer } from 'react-virtualized';
 
 import './RequestList.scss';
 
-const rowRenderer = (items) => ({
+const rowRenderer = (items, rowClick) => ({
         index,       // Index of row
-        isScrolling, // The List is currently being scrolled
-        isVisible,   // This row is visible within the List (eg it is not an overscanned row)
+        // isScrolling, // The List is currently being scrolled
+        // isVisible,   // This row is visible within the List (eg it is not an overscanned row)
         key,         // Unique key within array of rendered rows
-        parent,      // Reference to the parent List (instance)
+        // parent,      // Reference to the parent List (instance)
         style,        // Style object to be applied to row (to position it);        
-    }) => ( <div className="row" key={key} style={style}>{items[index].id}</div> );
+    }) => ( 
+        <div className={`row ${(index%2)?'odd':'even'} ${(rowClick)?'clickable':''}`} 
+                key={key} style={style} onClick={rowClick && rowClick(items[index].id)}>
+            <div className="request-id">#{items[index].id}</div>
+            <div className="request-payload">{JSON.stringify(items[index].requestPayload)}</div>
+            <div className="request-status">{items[index].status}</div>
+        </div>
+    );
 
 const RequestList = ({items, itemCount, onClick}) => (
-    <AutoSizer>
-        {({width, height}) => (
-            <List rowRenderer={rowRenderer(items)} 
-                rowCount={itemCount}
-                rowHeight={20}
-                height={height}
-                width={width}                
-            />
-        )}
-    </AutoSizer>
+    <div className="request-list">
+        <AutoSizer>
+            {({width, height}) => (
+                <List rowRenderer={rowRenderer(items)} 
+                    rowCount={itemCount}
+                    rowHeight={50}
+                    height={height}
+                    width={width}                
+                />
+            )}
+        </AutoSizer>
+    </div>
 );
 
 const mapStateToProps = (state) => {
